0e1698a353ba5c230b988f285923b515b4e4da37
Angel75Git <aduboncenteno@usf.edu>
2025-03-02 08:15:31 -0500

    Task 2 - Updated Metric Cards via Array Conversion

--- cc_12.js
+++ cc_12.js
@@ -20,3 +20,31 @@ revenueMetric.appendChild(title)
 revenueMetric.appendChild(placeHolder)      //Have revenueMetric adopt populated values
 
 metricDiv.appendChild(revenueMetric);    //Have metricDiv adopt revenueMetric
+
+//Task 2
+console.log("=".repeat(20));
+console.log("Task 2");
+//Creating Scenario with Profit and expense cards
+
+const profitCard = document.createElement("div"); //div creation
+const expenseCard = document.createElement("div");
+
+profitCard.setAttribute("class", "metric-card"); //set attributes
+expenseCard.setAttribute("class", "metric-card");
+
+profitCard.innerHTML = "<h3>Profit</h3> <p>$0</p>"  //More efficient population of values
+expenseCard.innerHTML = "<h3>Expenses</h3> <p>$0</p>"
+
+metricDiv.appendChild(profitCard); //Both cards are adopted by metricDiv
+metricDiv.appendChild(expenseCard);
+//Task 2 Requirements
+
+allMetrics = document.querySelectorAll(".metric-card")
+console.log(allMetrics); //Collecting all metric cards
+
+nodeListArray = Array.from(allMetrics)
+console.log(nodeListArray); //converting nodelist to array
+
+nodeListArray.forEach(card => {
+    card.style.background = "lightgreen"; //changing background color of cards
+});
